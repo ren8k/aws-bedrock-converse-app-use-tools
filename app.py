e521b7e6815882f02e6c46742c19cd4d5198326b
renya <rsmilek5492@gmail.com>
2024-05-31 12:17:00 +0900

    show history

--- app.py
+++ app.py
@@ -18,12 +18,9 @@ def generate_response(user_input, messages):
     bedrock_client = get_bedrock_client()
     system_prompts = [{"text": CFG.system_prompt}]
 
-    # Base inference parameters to use.
     inference_config = {"temperature": CFG.temperature}
-    # Additional inference parameters to use.
     additional_model_fields = {"top_k": CFG.top_k}
 
-    # Send the message.
     response = bedrock_client.converse(
         modelId=CFG.model_id,
         messages=messages,
@@ -41,20 +38,35 @@ def main():
     if "messages" not in st.session_state:
         st.session_state.messages = []
 
+    # Display chat messages from history on app rerun
+    for message in st.session_state.messages:
+        with st.chat_message(message["role"]):
+            print(message["content"])
+            st.write(content["text"] for content in message["content"])
+
     user_input = st.text_input("あなた: ", "")
 
     if st.button("送信") and user_input:
+        with st.chat_message("user"):
+            st.write(user_input)
         st.session_state.messages.append(
             {"role": "user", "content": [{"text": user_input}]}
         )
-        response_msg = generate_response(user_input, st.session_state.messages)
-        st.session_state.messages.append(response_msg)
 
-        st.write("アシスタント: ")
-        for content in response_msg["content"]:
-            st.write(content["text"])
-        print("#" * 50)
-        print(st.session_state.messages)
+        response_msg = generate_response(user_input, st.session_state.messages)
+        with st.chat_message("assistant"):
+            st.write(content["text"] for content in response_msg["content"])
+        st.session_state.messages.append(
+            {"role": "assistant", "content": response_msg["content"]}
+        )
+
+        # st.write("アシスタント: ")
+        # for content in response_msg["content"]:
+        #     st.write(content["text"])
+
+    # 会話履歴を画面に表示
+    print("#" * 50)
+    print(st.session_state.messages)
 
 
 if __name__ == "__main__":
