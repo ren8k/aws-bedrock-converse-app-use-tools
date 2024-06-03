32d9fa2b22a2bce7dae548467f81c543638e167e
renya <rsmilek5492@gmail.com>
2024-06-04 00:41:31 +0900

    add search tools

--- src/app/tools/tools_func.py
+++ src/app/tools/tools_func.py
@@ -1,6 +1,25 @@
+import requests
+from bs4 import BeautifulSoup
+from googlesearch import search
+
+
 class ToolsList:
     # Define our get_weather tool function...
     def get_weather(self, prefecture, city):
         result = f"Weather in {prefecture}, {city} is 70F and clear skies."
         print(f"Tool result: {result}")
         return result
+
+    # Define our web_search tool function...
+    def web_search(self, search_term):
+        results = []
+        response_list = []
+        results.extend([r for r in search(search_term, 3, "en")])
+        for j in results:
+            response = requests.get(j)
+            if response.status_code == 200:
+                soup = BeautifulSoup(response.text, "html.parser")
+                response_list.append(soup.get_text().strip())
+        response_text = ",".join(str(i) for i in response_list)
+        # print(f"Search results: {response_text}")
+        return response_text
