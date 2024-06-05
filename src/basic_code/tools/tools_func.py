267906a822360811868f1e3d9f9d97b585c96c64
renya <rsmilek5492@gmail.com>
2024-06-05 10:47:52 +0900

    add new files

---
+++ src/basic_code/tools/tools_func.py
@@ -0,0 +1,26 @@
+import requests
+from bs4 import BeautifulSoup
+from googlesearch import search
+
+
+class ToolsList:
+    # Define our get_weather tool function...
+    def get_weather(self, prefecture, city):
+        result = f"Weather in {prefecture}, {city} is 70F and clear skies."
+        print(f"Tool result: {result}")
+        return result
+
+    # Define our web_search tool function...
+    # reference: https://github.com/aws-samples/amazon-bedrock-samples/blob/b64902625ea8ade362c0f7d1978428cecdcf47ed/function-calling/Function%20calling%20tool%20use%20with%20Converse%20API.ipynb#L7
+    def web_search(self, search_term):
+        results = []
+        response_list = []
+        results.extend([r for r in search(search_term, 3, "en")])
+        for j in results:
+            response = requests.get(j)
+            if response.status_code == 200:
+                soup = BeautifulSoup(response.text, "html.parser")
+                response_list.append(soup.get_text().strip())
+        response_text = ",".join(str(i) for i in response_list)
+        # print(f"Search results: {response_text}")
+        return response_text
