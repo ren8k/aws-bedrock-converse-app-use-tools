119fb39346bd3d482f98ea16fa1595031aeeaef9
renya <rsmilek5492@gmail.com>
2024-06-21 01:53:52 +0900

    add claude 3.5 sonnet!!!

--- src/app/config/config.py
+++ src/app/config/config.py
@@ -45,6 +45,7 @@ class Config:
         "anthropic.claude-3-haiku-20240307-v1:0",
         "anthropic.claude-3-sonnet-20240229-v1:0",
         "anthropic.claude-3-opus-20240229-v1:0",
+        "anthropic.claude-3-5-sonnet-20240620-v1:0",
         "cohere.command-r-plus-v1:0",
         "cohere.command-r-v1:0",
         "mistral.mistral-large-2402-v1:0",
